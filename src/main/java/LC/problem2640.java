6d0a07a10c18677c7da904385c67a866f902509b
shivam855 <shivam906824@gmail.com>
2025-12-28 14:37:06 +0530

    LC 2640. Find the Score of All Prefixes of an Array

---
+++ src/main/java/LC/problem2640.java
@@ -0,0 +1,21 @@
+package LC;
+
+import java.util.Arrays;
+
+public class problem2640 {
+    public static void main(String[] args) {
+//        2640. Find the Score of All Prefixes of an Array
+        int [] nums = {1,1,2,4,8,16};
+        long [] arr = new long[nums.length];
+        int max = 0;
+        for(int i = 0 ; i < nums.length; i++)  {
+            max = Math.max(max, nums[i]);
+            if(i == 0) {
+                arr[i] = nums[i] + max;
+            } else {
+                arr[i] = nums[i] + max + arr[i - 1];
+            }
+        }
+        System.out.println(Arrays.toString(arr));
+    }
+}
